8e562a485ed1e5c3805ee703265e63f615e0601b
KirillMishkin <mkv1005@gmail.com>
2020-01-24 15:46:25 +0300

    exercise5 24012020

--- exercise5-ls6.py
+++ exercise5-ls6.py
@@ -3,4 +3,34 @@
 # Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw. Для каждого из
 # классов метод должен выводить уникальное сообщение. Создать экземпляры классов и проверить, что выведет описанный
 # метод для каждого экземпляра.
+from time import sleep
 
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw_method(self):
+        sleep(1)
+        print(f'Запуск отрисовки {self.title}')
+
+
+class Pen(Stationery):
+    pass
+
+
+class Pencil(Stationery):
+    pass
+
+
+class Handle(Stationery):
+    pass
+
+
+go_pen = Pen('Pen(ручка)')
+go_pen.draw_method()
+
+go_pencil = Pencil('Pencil(карандаш)')
+go_pencil.draw_method()
+
+go_handle = Handle('Handle(маркер)')
+go_handle.draw_method()
